4fd74497d18a1f97857d748c789b55ca7f79cf28
Ashwin Naren <arihant2math@gmail.com>
2025-03-29 13:11:20 -0700

    Fix Tcl_DecrRefCount and Tcl_IncrRefCount

--- tk-sys/tests/simple.rs
+++ tk-sys/tests/simple.rs
@@ -1,8 +1,26 @@
+fn convert_str(s: &str) -> *const std::ffi::c_char {
+    s.as_ptr() as *const std::ffi::c_char
+}
+
 #[test]
 fn test_basic() {
     unsafe {
         let interp = tk_sys::Tcl_CreateInterp();
         assert!(!interp.is_null());
+        tk_sys::Tcl_DeleteInterp(interp);
+    }
+}
+
+#[test]
+fn test_mem() {
+    unsafe {
+        let interp = tk_sys::Tcl_CreateInterp();
+        let mut len = 0;
+        let ptr = tk_sys::Tcl_Alloc(100);
+        assert!(!ptr.is_null());
+        tk_sys::Tcl_Free(ptr);
+        let mut value = *tk_sys::Tcl_NewStringObj(convert_str("true"), -1);
+        tk_sys::Tcl_DecrRefCount(&mut value);
         tk_sys::Tcl_DeleteInterp(interp);
     }
 }

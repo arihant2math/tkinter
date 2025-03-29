cb7bcfacad7046d014d548dccdabd9ec52c96e42
Ashwin Naren <arihant2math@gmail.com>
2025-03-29 13:30:04 -0700

    add missing file

--- tk-sys/src/common_func_macros.rs
+++ tk-sys/src/common_func_macros.rs
@@ -1,13 +1,1 @@
-fn Tcl_DecrRefCount(objPtr: *mut Tcl_Obj) {
-    let mut obj = unsafe { *objPtr };
-    obj.refCount -= 1;
-    if obj.refCount == 0 {
-        // Free the object
-        unsafe { Tcl_Free(objPtr as *mut std::ffi::c_char) };
-    }
-}
-
-fn Tcl_IncrRefCount(objPtr: *mut Tcl_Obj) {
-    let mut obj = unsafe { *objPtr };
-    obj.refCount += 1;
-}
+a

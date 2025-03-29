b9b1fc10196884a7e3eec784190f64ab5e56bd60
Ashwin Naren <arihant2math@gmail.com>
2025-03-29 13:32:30 -0700

    attem fix

--- tcl-sys/src/lib.rs
+++ tcl-sys/src/lib.rs
@@ -4,4 +4,16 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
-include!("common_func_macros.rs");
+pub fn Tcl_DecrRefCount(objPtr: *mut Tcl_Obj) {
+    let mut obj = unsafe { *objPtr };
+    obj.refCount -= 1;
+    if obj.refCount == 0 {
+        // Free the object
+        unsafe { Tcl_Free(objPtr as *mut std::ffi::c_char) };
+    }
+}
+
+pub fn Tcl_IncrRefCount(objPtr: *mut Tcl_Obj) {
+    let mut obj = unsafe { *objPtr };
+    obj.refCount += 1;
+}

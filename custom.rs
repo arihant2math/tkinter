8e8d7aa6684b748a1b801282fe191e3e79ef4682
Ashwin Naren <arihant2math@gmail.com>
2025-04-07 21:39:17 -0700

    Inline th refcount functions

--- custom.rs
+++ custom.rs
@@ -4,6 +4,7 @@
 /// # Safety
 /// If the reference count is decremented to zero, the object is freed and `objPtr` turns into a null pointer.
 /// If objPtr is null, UB occurs.
+#[inline(always)]
 pub unsafe fn Tcl_DecrRefCount(objPtr: *mut Tcl_Obj) {
     let mut obj = unsafe { *objPtr };
     obj.refCount -= 1;
@@ -19,6 +20,7 @@ pub unsafe fn Tcl_DecrRefCount(objPtr: *mut Tcl_Obj) {
 /// # Safety
 /// If [`Tcl_DecrRefCount`] is not called and the pointer is dropped, the object will be leaked.
 /// If objPtr is null, UB occurs.
+#[inline(always)]
 pub unsafe fn Tcl_IncrRefCount(objPtr: *mut Tcl_Obj) {
     let mut obj = unsafe { *objPtr };
     obj.refCount += 1;
